5d410f95f9f759ed420c4996965297b79d5e60c0
Daniel Hermon <danielhermonsync@gmail.com>
2021-01-19 13:55:32 +0200

    style: fix prettier

--- tests/unit/schema/schemaManager/mapping.spec.ts
+++ tests/unit/schema/schemaManager/mapping.spec.ts
@@ -1,18 +1,18 @@
 import { Schemas } from '../../../../src/schema/models/mapping';
 
-describe('Schema', function() {
-    describe('#getSchemas', function() {
-        it('should return an empty array', async function() {
-            const schema = new Schemas();
-            const schemaMapping = await schema.getSchemas();
-            expect(schemaMapping).toEqual([]);
-        });
+describe('Schema', function () {
+  describe('#getSchemas', function () {
+    it('should return an empty array', async function () {
+      const schema = new Schemas();
+      const schemaMapping = await schema.getSchemas();
+      expect(schemaMapping).toEqual([]);
     });
-    describe('#getSchema', function() {
-        it('should return undefined', async function() {
-            const schema = new Schemas();
-            const schemaMapping = await schema.getSchema('some_schema_name');
-            expect(schemaMapping).toBeUndefined();
-        })
+  });
+  describe('#getSchema', function () {
+    it('should return undefined', async function () {
+      const schema = new Schemas();
+      const schemaMapping = await schema.getSchema('some_schema_name');
+      expect(schemaMapping).toBeUndefined();
     });
+  });
 });
