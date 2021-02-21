d2c52ad5b6114553b193ba0cf165c7610e02e179
galta95 <48960890+galta95@users.noreply.github.com>
2021-02-21 12:19:14 +0200

    refactor(schemas): add temporary hard-coded schema (#27)
    * refactor(schemas): add temporary hard-coded schema
    
    * style(schemas): run prettier

--- tests/unit/schema/schemaManager/mapping.spec.ts
+++ tests/unit/schema/schemaManager/mapping.spec.ts
@@ -2,10 +2,18 @@ import { Schemas } from '../../../../src/schema/models/mapping';
 
 describe('Schema', function () {
   describe('#getSchemas', function () {
-    it('should return an empty array', async function () {
+    it('should return avi hard-coded schema - temporary test', async function () {
       const schema = new Schemas();
+      const expected = [
+        {
+          name: 'avi',
+          createdAt: '2020-12-20T13:32:25Z',
+          updatedAt: '2020-12-20T13:32:25Z',
+          mapping: {},
+        },
+      ];
       const schemaMapping = await schema.getSchemas();
-      expect(schemaMapping).toEqual([]);
+      expect(schemaMapping).toEqual(expected);
     });
   });
   describe('#getSchema', function () {
