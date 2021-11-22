f23d73f8ac1f9c295c494f844fd66008b65e6d33
Vitali Ginzburg <54726763+vitaligi@users.noreply.github.com>
2021-11-23 01:02:29 +0200

    refactor: remove connection string usage

--- src/common/db.ts
+++ src/common/db.ts
@@ -1,27 +1,19 @@
 import { Logger } from '@map-colonies/js-logger';
 import Redis, { RedisOptions } from 'ioredis';
 import { container } from 'tsyringe';
-import { HOSTNAME, REDIS_CONNECTION_ERROR_CODE, SERVICES } from './constants';
+import { HOSTNAME, SERVICES } from './constants';
 
-export const createConnection = async (dbConfig: RedisOptions | string): Promise<Redis.Redis> => {
+export const createConnection = async (redisOptions: RedisOptions): Promise<Redis.Redis> => {
   try {
-    let redis: Redis.Redis;
-    if (typeof dbConfig === 'object') {
-      dbConfig = {
-        ...dbConfig,
-        retryStrategy: (): null => null,
-        reconnectOnError: (): 1 => 1,
-        lazyConnect: true,
-        connectionName: HOSTNAME,
-      };
-      redis = new Redis(dbConfig);
-    } else {
-      const url = new URL(dbConfig);
-      if (url.searchParams.get('lazyConnect') === null) {
-        url.searchParams.set('lazyConnect', 'true');
-      }
-      redis = new Redis(url.href);
-    }
+    redisOptions = {
+      ...redisOptions,
+      retryStrategy: (): null => null,
+      reconnectOnError: (): 1 => 1,
+      lazyConnect: true,
+      connectionName: HOSTNAME,
+    };
+    const redis: Redis.Redis = new Redis(redisOptions);
+
     await redis.connect();
     return redis;
   } catch (e) {
