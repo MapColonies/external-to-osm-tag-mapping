581b05bbc0c5ef4aed291b07616737a3cb5e9d3a
Eyal Rozen <u9417205@bsmch.net>
2026-02-15 16:25:22 +0200

    fix: remove unnecessary redis manager functions

--- src/schema/DAL/redisManager.ts
+++ src/schema/DAL/redisManager.ts
@@ -7,7 +7,6 @@ import { IDomainFieldsRepository } from './domainFieldsRepository';
 @injectable()
 export class RedisManager implements IDomainFieldsRepository {
   public getData: (fields: string[]) => Promise<(string | null)[]>;
-  public getAll: () => Promise<Record<string, string>>;
 
   public constructor(
     @inject(REDIS_SYMBOL) public readonly redisInstance: redis,
@@ -20,35 +19,9 @@ export class RedisManager implements IDomainFieldsRepository {
         const result = await this.redisInstance.hmget(value, ...fields);
         return result;
       };
-
-      this.getAll = async (): Promise<Record<string, string>> => {
-        await this.redisInstance.hset('kaki', { a: '1', b: '2' });
-        return this.redisInstance.hgetall('hitelr');
-      };
     } else {
       this.getData = async (fields: string[]): Promise<(string | null)[]> => {
         const result = await this.redisInstance.mget(fields);
-        return result;
-      };
-
-      this.getAll = async (): Promise<Record<string, string>> => {
-        const keys = await this.redisInstance.keys('*');
-
-        if (keys.length === 0) {
-          return {};
-        }
-
-        const values = await this.redisInstance.mget(keys);
-
-        const result: Record<string, string> = {};
-
-        keys.forEach((key, index) => {
-          const value = values[index];
-          if (value !== null) {
-            result[key] = value;
-          }
-        });
-
         return result;
       };
     }
@@ -57,16 +30,6 @@ export class RedisManager implements IDomainFieldsRepository {
   public async getFields(fields: string[]): Promise<(string | null)[]> {
     try {
       const result = await this.getData(fields);
-      return result;
-    } catch (error) {
-      console.error('GetFields error:', error);
-      throw new Error('redis: failed to fetch keys ');
-    }
-  }
-
-  public async getAllTable(): Promise<Record<string, string>> {
-    try {
-      const result = await this.getAll();
       return result;
     } catch (error) {
       console.error('GetFields error:', error);
